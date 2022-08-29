d37abc990ad812c35ee95d9642f05667adcf20b9
kcorbin01 <kcorbin01@huskers.unl.edu>
2022-08-29 22:07:08 +0000

    add major and name to partners file

--- hello.c
+++ hello.c
@@ -4,7 +4,10 @@
 
 int main(int argc, char **argv) {
     printf("Hello, My name is Wesley Buryanek!\n");
-    printf("My Major is Computer Science!\n");
+    printf("My Major is Computer Science!\n\n");
+
+    printf("Hello, I'm Konner!\n");
+    printf("Major is Computer Engineering!\n");
 
     return 0;
 }
