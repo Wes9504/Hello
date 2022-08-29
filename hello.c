db9526b6f1a985b015e37ac011cfbd0d433e1bab
Wes9504 <111804037+Wes9504@users.noreply.github.com>
2022-08-29 21:22:19 +0000

    added hello.c

---
+++ hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char **argv) {
+    printf("Hello, My name is Wesley Buryanek!\n");
+    printf("My Major is Computer Science!\n");
+
+    return 0;
+}
